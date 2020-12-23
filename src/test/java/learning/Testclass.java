082d6985a362fe15e8ddea580a9457221b9ff385
Anand Mishra <anand.mishra@irissoftware.com>
2020-12-23 14:00:12 +0530

    source commit

---
+++ src/test/java/learning/Testclass.java
@@ -0,0 +1,14 @@
+package learning;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.Test;
+
+public class Testclass {
+
+	@Test
+	public void test() {
+		assertEquals(true, true);
+	}
+
+}
